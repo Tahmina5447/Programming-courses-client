aa2ce91fd4c283bf97f6cf8686361104f9d6dabe
Tahmina <tahminakhatun5447@gmail.com>
2022-10-24 21:36:55 +0600

    add site logo and set title

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>from app js</h1>
-      <button className='btn'>hello</button>
+    <div>
+      
     </div>
   );
 }
