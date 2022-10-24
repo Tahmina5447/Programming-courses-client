c94b66a4242a2edd546fdd8ab239759f17e19506
Tahmina <tahminakhatun5447@gmail.com>
2022-10-24 21:29:35 +0600

    install tailwind and daisiui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>from app js</h1>
+      <button className='btn'>hello</button>
     </div>
   );
 }
